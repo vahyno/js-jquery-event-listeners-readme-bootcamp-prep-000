6ae14767075adbae68245cb1d6204ab5fe91a868
M V <vahyno+learn@gmail.com>
2017-09-29 06:07:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $(this).addClass( "tasty" );
+  });
+}
+
+function pressIt(){
+  $('form').on("keydown", function(key){
+    if(key.which == 71){
+      alert('Letter G was pressed');
+  }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+    //if ($( "input:first" ).val() === "correct") {
+    alert("Your form is going to be submitted now.");
+    /*return;
+    }
+    alert("you entered the wrong value");
+    return;*/
+  });
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
